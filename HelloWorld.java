7f373c0c34c56325b152a427cbfbc6d2cc8be917
shibu <shibuyabrutal.yahoo.co.jp>
2023-11-25 17:06:20 +0900

    Fix: オリジナルプリンントメソッドを追加

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+
+  public static void HelloKaneda() {
+    System.out.println("俺金田");
+  }
 }
